57cda156e241d4b5ceb803f7bea606914a32a6df
GOVERNET\mmcguff <mmcguff@governet.net>
2017-09-05 13:18:01 -0600

    So the jquery in the javascript.js actually returns the location of the from the api.  You will need to develop this further so that works as you desire and makes good sense.  Keeping working with this.   This should build fairly quickly.

--- javascript.js
+++ javascript.js
@@ -22,10 +22,39 @@ function setPosition(position) {
 
     //need to take this url and generate the stuff you will need to get your wather app working. 
     var json = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long;
-    alert(json);
+    $(document).ready(function() {
+        var lang = '';
+    var obj = $.parseJSON(json);
+    $.each(obj, function() {
+        lang = this['name'] ;
+    });
+//    $('demo').html(lang);
+alert(lang);
+});
 }
 
+//everything you do needs to feed this function below. 
+  $(document).ready(function () {
+        $("#getWeather").on("click", function () {
+            var _lat  = 45;
+            var _long = -111;
+
+            var url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + _lat + '&lon=' + _long;
+            
+            alert(url);
+
+            $.getJSON(url, function (a) {
+               
+               
+               
+                $("#weather").html("<p>" + a.name + "</p>")
+            });
+
+        });
+    
+  });
 
 
+  
 
 
