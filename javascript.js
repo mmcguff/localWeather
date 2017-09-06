9bd759a227b87295e8fadf4f483e31271efc2998
GOVERNET\mmcguff <mmcguff@governet.net>
2017-09-06 10:52:29 -0600

    Got the both location and temperature populating.  When time permits start getting more data to populate.

--- javascript.js
+++ javascript.js
@@ -1,60 +1,53 @@
 
     
-
-    
 var x = document.getElementById("demo");
-var lat;
-var long;
 
-//Get the users location
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(setPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-//Find the weather in that location.  Plug into a weather API to get the
-function setPosition(position) {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
 
-    //need to take this url and generate the stuff you will need to get your wather app working. 
-    var json = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long;
-    $(document).ready(function() {
-        var lang = '';
-    var obj = $.parseJSON(json);
-    $.each(obj, function() {
-        lang = this['name'] ;
-    });
-//    $('demo').html(lang);
-alert(lang);
-});
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var obj = JSON.parse(this.responseText);
+       
+        document.getElementById("fcc_location").innerHTML = obj.name;
+        document.getElementById("fcc_temp").innerHTML = obj.main.temp + " C";
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();    
+    //url = JSON.stringify(url);
+    //var obj = JSON.parse();
+
+  //  document.getElementById("weather").innerHTML = url;//obj.name;
 }
 
-//everything you do needs to feed this function below. 
-  $(document).ready(function () {
-        $("#getWeather").on("click", function () {
-            var _lat  = 45;
-            var _long = -111;
-
-            var url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + _lat + '&lon=' + _long;
-            
-            alert(url);
-
-            $.getJSON(url, function (a) {
-               
-               
-               
-                $("#weather").html("<p>" + a.name + "</p>")
-            });
-
-        });
-    
-  });
 
 
-  
-
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
 
