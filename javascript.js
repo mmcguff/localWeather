325b3944f92bb971165adb941f15449ffe7a82f0
GOVERNET\mmcguff <mmcguff@governet.net>
2017-09-06 14:23:38 -0600

    Need to change some labels but I added a function to caculate wind direction.  Looking pretty good right now.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 
     
-var x = document.getElementById("demo");
-
+var x = '';
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -15,6 +14,7 @@ function showPosition(position) {
     var lon = position.coords.longitude;
 
     var url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon;
+    console.log(url);
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -23,9 +23,10 @@ function showPosition(position) {
         document.getElementById("fcc_location").innerHTML = obj.name;
         document.getElementById("time").innerHTML = getTime();
         document.getElementById("fcc_description").innerHTML = obj.weather[0].description;
-        document.getElementById("fcc_weather_icon").innerHTML = JSON.parse(obj.weather[0].icon);
-   
-        document.getElementById("fcc_temp").innerHTML = obj.main.temp + " C";
+        document.getElementById("fcc_weather_icon").innerHTML ="<img src=" + obj.weather[0].icon + ">";
+        document.getElementById("fcc_humidity").innerHTML = obj.main.humidity + "%";
+        document.getElementById("fcc_wind_speed").innerHTML = obj.wind.speed + " mph  " + getWindDirection(obj.wind.deg);
+        document.getElementById("fcc_temp").innerHTML = obj.main.temp + "°C";
     }
 };
 xmlhttp.open("GET", url, true);
@@ -74,3 +75,31 @@ function getTime()
 
 }
 
+function getWindDirection(deg)
+{
+   
+var wBearing = deg;
+
+var wDir = "";
+if (wBearing >= 348.75 || wBearing < 11.25)  { return wDir = "N";} else
+if (wBearing >= 11.25 && wBearing < 33.75)   { return wDir = "NNE";} else
+if (wBearing >= 33.75 && wBearing < 56.25)   { return wDir = "NE";} else
+if (wBearing >= 56.25 && wBearing < 78.75)   { return wDir = "ENE";} else
+if (wBearing >= 78.75 && wBearing < 101.25)  { return wDir = "E";} else
+if (wBearing >= 101.25 && wBearing < 123.75) { return wDir = "ESE";} else
+if (wBearing >= 123.75 && wBearing < 146.25) { return wDir = "SE";} else
+if (wBearing >= 146.25 && wBearing < 168.75) { return wDir = "SSE";} else
+if (wBearing >= 186.75 && wBearing < 191.25) { return wDir = "S";} else
+if (wBearing >= 191.25 && wBearing < 213.75) { return wDir = "SSW";} else
+if (wBearing >= 213.75 && wBearing < 236.25) { return wDir ="SW";} else
+if (wBearing >= 236.25 && wBearing < 258.75) { return wDir = "WSW";} else
+if (wBearing >= 258.75 && wBearing < 281.25) { return wDir = "W" ;} else
+if (wBearing >= 281.25 && wBearing < 303.75) { return wDir = "WNW";} else
+if (wBearing >= 303.75 && wBearing < 326.25) { return wDir = "NW";} else
+                                             { return wDir = "NNW";};
+}
+
+
+//lets create a convert to Farenthiet function as well. 
+
+//lets include the wind direction function also. 
